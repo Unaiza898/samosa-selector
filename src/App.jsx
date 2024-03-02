c58f3c0fdd84a17041a2c36487bfad665f589489
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2024-03-02 15:08:58 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,52 @@ const App = () => {
   
   const updateCount = () => setCount(count + multiplier);
 
+  const buyDoubleStuffed = () =>{
+    if(count >= 10){
+      setMultiplier(multiplier * 2);
+      setCount(count - 10)
+    }
+  }
+
+  const buyPartyPack = () => {
+    if(count >= 100) {
+      setMultiplier(multiplier * 5)
+      setCount(count - 100)
+    }
+  }
+
+  const buyFullFeast = () => {
+    if(count >= 1000){
+      setMultiplier(multiplier *10)
+      setCount(count - 1000)
+    }
+  }
   return (
     <div className="App">
       <div className='header'>
         <h1> Samosa Selector</h1>
         <h2>Count: {count} </h2>
         <img src={samosa} className='samosa' onClick={updateCount}/>
-
-        
-        
-
-
       </div>
-
+      <div className = "container">
+        <div className = "upgrade">
+          <h3>Double Stuffed</h3>
+          <p>2x per click</p>
+          <button onClick={buyDoubleStuffed}>10 Samosas</button>
+        </div>
+        <div className = "upgrade">
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button onClick={buyPartyPack}>100 Samosas</button>
+        </div>
+        <div className = "upgrade">
+          <h3>Full Feast 👩‍🍳</h3>
+          <p>10x per click</p>
+          <button onClick={buyFullFeast}>1000 Samosas</button>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default App
+export default App;
